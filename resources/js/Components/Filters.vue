c49177e801af2bfd8ceab7ea45c8ef75f664ebcb
gpanos <karapanos.dimitris@gmail.com>
2022-03-24 17:27:11 +0100

    wip

--- resources/js/Components/Filters.vue
+++ resources/js/Components/Filters.vue
@@ -10,7 +10,6 @@ defineEmits(['update:search', 'reset'])
   <div class="flex items-center justify-between">
     <div class="flex w-full bg-white rounded shadow">
       <input class="relative px-6 py-3 w-full rounded-r focus:shadow-outline" autocomplete="off" type="text" name="search" placeholder="Search…" :value="search" @input="$emit('update:search', $event.target.value)" />
-      <!-- TODO: Add duration filter -->
     </div>
     <button class="ml-3 text-gray-500 hover:text-gray-700 focus:text-blue-800 text-sm" type="button" @click="$emit('reset')">Reset</button>
   </div>
